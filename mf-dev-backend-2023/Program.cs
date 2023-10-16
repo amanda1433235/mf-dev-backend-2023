3697d7aff0e84244e46176a70776cf4688768669
amanda1433235 <amanda.gomes.1433235@sga.pucminas.br>
2023-10-16 18:07:33 -0300

    Configuração do Banco de dados e da Tabela Veículo

--- mf-dev-backend-2023/Program.cs
+++ mf-dev-backend-2023/Program.cs
@@ -1,3 +1,6 @@
+using mf_dev_backend_2023.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace mf_dev_backend_2023
 {
     public class Program
@@ -10,6 +13,9 @@ namespace mf_dev_backend_2023
             builder.Services.AddControllersWithViews();
 
             builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            builder.Services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
